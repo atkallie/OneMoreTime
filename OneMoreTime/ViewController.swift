ab6d6171cfaa62ac3a4970b70dbc35983e33b59d
atkallie <kal17da@gmail.com>
2017-01-31 13:19:45 -0500

    Initial Commit

--- OneMoreTime/ViewController.swift
+++ OneMoreTime/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var ageEntered: UITextField!
+    @IBOutlet var ageDisplay: UILabel!
+    
+    @IBAction func calculator(_ sender: Any) {
+        
+        ageDisplay.text = String(Int(ageEntered.text!)!*7)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
